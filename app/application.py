09db6485147ba125a65bfec8815c0f6167bd5790
Hasan Jafri <hasan.jafri7@gmail.com>
2018-12-16 18:37:22 -0500

    WIP: Supreme Tab

--- app/application.py
+++ app/application.py
@@ -1,11 +1,23 @@
 from kivy.app import App
 from kivy.uix.tabbedpanel import TabbedPanel
-from kivy.uix.dropdown import DropDown
 
-class ProductListDropDown(DropDown):
-    pass
 class AIOTabbed(TabbedPanel):
-    pass
+    def __init__(self, **kwargs):
+        self.getSupremeProductTypes()
+        super(AIOTabbed, self).__init__(**kwargs)
+
+    def getSupremeProductTypes(self):
+        self.supremeProductTypes = ['Jackets', 'Shirts', 'Tops/Sweaters', 'Sweatshirts', 'Pants', 'T-Shirts', 'Hats', 'Accessories', 'Shoes', 'Skate']
+
+    def updateProductsTypeSpinner(self, text):
+
+        for productType in self.supremeProductTypes:
+            if productType == text:
+                self.ids.productsType_spinner.text = text
+
+    def updateProductKeyword(self, text):
+        print(self.ids.productKeyword_ti.text)
+        self.ids.productKeyword_ti.text = text
 
 class AIOKivyApp(App):
     def build(self):
